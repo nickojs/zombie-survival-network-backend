26e0e9f7f59d1f4bcdc0d39f6ae7a9462305b760
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-22 15:25:58 -0300

    feat(entity/user): add validation to User

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,3 +1,4 @@
+import { IsEmail, Length, MinLength } from 'class-validator';
 import { Column, Entity, PrimaryGeneratedColumn } from 'typeorm';
 
 @Entity()
@@ -5,12 +6,19 @@ export class User {
   @PrimaryGeneratedColumn('uuid')
   id: string;
 
+  @Length(4, 16, {
+    message: 'Username should range between 4 and 16 characters'
+  })
   @Column({ unique: true })
   username: string;
 
+  @IsEmail({ }, { message: 'Invalid email' })
   @Column({ unique: true })
   email: string;
 
+  @MinLength(8, {
+    message: 'Password should have at least 8 characters'
+  })
   @Column()
   password: string;
 }
