5085f5c7a41fac1f066a9e87b1bf2f299ab26616
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-23 10:49:51 -0300

    feat(controllers/auth): create base for signUp method

---
+++ src/controllers/AuthController.ts
@@ -0,0 +1,48 @@
+import {
+  Body, HttpError, JsonController, Post
+} from 'routing-controllers';
+import { getRepository } from 'typeorm';
+import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
+import { User } from '../entity/User';
+
+interface SignInProps {
+  email?: string;
+  username?: string;
+  password: string;
+}
+
+@JsonController('/auth')
+export class AuthController {
+  private userRepository = getRepository(User)
+
+  @Post('/signin')
+  async signIn(@Body() user: SignInProps) {
+    const { email, username, password } = user;
+
+    const findUser = await this.userRepository.findOne({
+      where: [
+        { email },
+        { username }
+      ]
+    });
+
+    if (!findUser || findUser === undefined) throw new HttpError(404, 'Could not find user');
+    const checkPw = await this.comparePassword(password, findUser.password);
+
+    if (!checkPw) throw new HttpError(401, 'Invalid credentials');
+
+    return { token: this.generateJwt(findUser) };
+  }
+
+  private comparePassword = async (
+    passwordToCompare: string,
+    existingPassword: string
+  ) => bcrypt.compare(passwordToCompare, existingPassword)
+
+  private generateJwt = (user: User) => jwt.sign({
+    userId: user.id,
+    email: user.email,
+    username: user.username
+  }, process.env.secret)
+}
