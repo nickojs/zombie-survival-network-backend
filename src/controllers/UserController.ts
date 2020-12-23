a79ef1d90ca204ac9c7509528a54844d5495448a
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-23 08:37:11 -0300

    fix(controller/user): add hashPassword method, fix @Body() return type

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,7 +1,10 @@
 import {
-  Body, Get, JsonController, Post
+  Body, Get, JsonController, Post, Res
 } from 'routing-controllers';
+import { validate, ValidationError } from 'class-validator';
 import { getRepository } from 'typeorm';
+import { Response } from 'express';
+import * as bcrypt from 'bcrypt';
 import { User } from '../entity/User';
 
 @JsonController('/user')
@@ -14,7 +17,37 @@ export class UserControler {
   }
 
   @Post('/')
-  async createUser(@Body() user: User) {
-    await this.userRepository.save(user);
+  async createUser(
+    @Body() user: Record<string, any>,
+    @Res() res: Response
+  ) {
+    const createUser = this.userRepository.create(user);
+
+    const errors = await validate(createUser);
+    if (errors.length > 0) {
+      return res.status(422).json({
+        errors: this.parseErrors(errors)
+      });
+    }
+
+    const hashedPw = await this.hashPassword(createUser.password);
+    createUser.password = hashedPw;
+    await this.userRepository.save(createUser);
+
+    return res.status(201).json({ message: 'Created user' });
   }
+
+  private parseErrors(errors: ValidationError[]) {
+    return errors.map((e) => ({
+      property: e.property,
+      constraints: e.constraints
+    }));
+  }
+
+  private hashPassword = async (pw: string) => bcrypt.hash(pw, 12);
+
+  private comparePassword = async (
+    passwordToCompare: string,
+    existingPassword: string
+  ) => bcrypt.compare(passwordToCompare, existingPassword)
 }
