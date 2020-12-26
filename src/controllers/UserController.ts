9c974aca06f646521c9c30422975af3bce92a525
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2020-12-26 20:25:46 -0300

    feat(controllers/user): create editUserProfile method to create/edit UserProfile

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,15 +1,18 @@
 import {
-  Body, Get, HttpError, JsonController, Post, Res
+  Body, CurrentUser, Get, HttpError, JsonController, Post, Put, Res
 } from 'routing-controllers';
 import { validate, ValidationError } from 'class-validator';
 import { getRepository } from 'typeorm';
 import { Response } from 'express';
 import * as bcrypt from 'bcrypt';
 import { User } from '../entity/User';
+import { UserProfile } from '../entity/Profile';
 
 @JsonController('/user')
 export class UserController {
   private userRepository = getRepository(User)
+
+  private userProfileRepository = getRepository(UserProfile);
 
   @Get('/')
   async getUsers() {
@@ -33,6 +36,46 @@ export class UserController {
     await this.userRepository.save(createUser);
 
     return res.status(201).json({ message: 'Created user' });
+  }
+
+  @Put('/profile')
+  async editUserProfile(
+    @Body() data: Record<string, any>,
+    @CurrentUser() userId: string
+  ) {
+    const user = await this.userRepository.findOne({
+      where: { id: userId },
+      relations: ['profile']
+    });
+
+    const userProfile = this.userProfileRepository.create({
+      ...data
+    });
+
+    const errors = await validate(userProfile);
+    if (errors.length > 0) {
+      throw new HttpError(422, this.parseErrors(errors));
+    }
+
+    const { profile } = user || { };
+    // updates existing profile
+    if (profile) {
+      const updatedProfile = { ...profile, ...userProfile };
+      await this.userProfileRepository.save(updatedProfile);
+      return {
+        message: 'Updated profile',
+        profile: updatedProfile
+      };
+    }
+
+    await this.userProfileRepository.save(userProfile);
+    user.profile = userProfile;
+    await this.userRepository.save(user);
+
+    return {
+      message: 'Created profile for this user',
+      profile: userProfile
+    };
   }
 
   private parseErrors(errors: ValidationError[]) {
