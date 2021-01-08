51ea9742dda043d75f033ca1b16004c6f2c2b7b4
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-08 03:48:30 -0300

    feat(controllers/inventory): create 'saveItemToInventory' method

--- src/controllers/InventoryController.ts
+++ src/controllers/InventoryController.ts
@@ -3,7 +3,7 @@ import {
 } from 'routing-controllers';
 import { getRepository } from 'typeorm';
 import * as bodyParser from 'body-parser';
-import { } from '../middleware/validate';
+import { validateItem } from '../middleware/validate';
 import { User } from '../entity/User';
 import { Item } from '../entity/Item';
 
@@ -26,5 +26,31 @@ export class InventoryController {
     const { items } = fetchUser;
 
     return items;
+  }
+
+  @Post('/')
+  @UseBefore(bodyParser.json(), validateItem)
+  async saveItemToInventory(
+    @CurrentUser({ required: true }) user: User,
+    @Body() data: Record<string, any>
+  ) {
+    const item = this.itemRepository.create(data);
+    const { items } = user;
+
+    const existingItem = items.find((i) => i.OSRSId === item.OSRSId);
+    if (existingItem) {
+      const { qtd } = existingItem;
+      if ((qtd + item.qtd) > 28) throw new HttpError(422, 'You can\'t have more than 28 items of the same type');
+
+      existingItem.qtd += item.qtd;
+      await this.itemRepository.save(existingItem);
+      return { message: `Incremented ${existingItem.OSRSId} quantity` };
+    }
+
+    await this.itemRepository.save(item);
+    user.items = [...user.items, item];
+    await this.userRepository.save(user);
+
+    return { message: 'Saved item to inventory' };
   }
 }
