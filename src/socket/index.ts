a4b20f7bb59be8dfb0f2b4df50f5f7976220f119
Nicolau Barsotti <62088609+nickojs@users.noreply.github.com>
2021-01-12 18:22:50 -0300

    feat(socket): add more listeners and events

--- src/socket/index.ts
+++ src/socket/index.ts
@@ -14,6 +14,7 @@ enum SocketEvents {
   SENDER_ACKNOWLEDGE = 'sender_acknowledge',
   RECIPIENT_ACKNOWLEDGE = 'recipient_acknowledge',
   REQUEST_USER_STATUS = 'request_user_status',
+  RECIPIENT_STATUS = 'recipient_status'
 }
 
 interface ConnectedUser {
@@ -65,95 +66,97 @@ const retrieveUsers = (recipientId: string, senderId: string) => {
 
 const resetUser = (user: ConnectedUser) => userFactory(user.userId, user.socketId);
 
-try {
-  socket.on(SocketEvents.CONNECTION, (socket: Socket) => {
-    console.log(`connected : ${socket.id}`);
+socket.on(SocketEvents.CONNECTION, (socket: Socket) => {
+  console.log(`connected : ${socket.id}`);
 
-    socket.on(SocketEvents.SAVE_USER, (userId: string) => {
-      if (userId && existingUser(userId) === -1) {
-        const user = userFactory(userId, socket.id);
-        connectedUsers.push(user);
+  socket.on(SocketEvents.SAVE_USER, (userId: string) => {
+    if (userId && existingUser(userId) === -1) {
+      const user = userFactory(userId, socket.id);
+      connectedUsers.push(user);
+    }
+  });
+
+  socket.on(SocketEvents.RECIPIENT_STATUS, (data: Partial<SendItemData>) => {
+    const { survivor } = data;
+    const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
+    socket.emit(SocketEvents.RECIPIENT_STATUS, recipient);
+  });
+
+  socket.on(SocketEvents.OPEN_TRADE, (
+    data: { survivor: Partial<User>, sender: string }
+  ) => {
+    const { survivor } = data;
+    const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
+    sender.trading = {
+      isTrading: true,
+      withWho: recipient.socketId
+    };
+
+    if (!recipient.trading.isTrading) {
+      socket.to(recipient.socketId).emit(SocketEvents.OPEN_TRADE, `${sender.userId} wants to trade`);
+    }
+  });
+
+  socket.on(SocketEvents.SEND_ITEMS, (data: SendItemData) => {
+    const { items, survivor } = data;
+    const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
+
+    if (recipient && sender) {
+      // check if either of sender or recipient is already on a trade
+      if (recipient.trading.withWho !== socket.id) {
+        return socket.emit(SocketEvents.DECLINE_EXISTING_TRADE, 'This user is already on a trade');
       }
-    });
-
-    socket.on(SocketEvents.OPEN_TRADE, (
-      data: { survivor: Partial<User>, sender: string }
-    ) => {
-      const { survivor } = data;
-      const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
-      sender.trading = {
-        isTrading: true,
-        withWho: recipient.socketId
-      };
-
-      if (!recipient.trading.isTrading) {
-        socket.to(recipient.socketId).emit(SocketEvents.OPEN_TRADE, `${sender.userId} wants to trade`);
-      }
-    });
-
-    socket.on(SocketEvents.SEND_ITEMS, (data: SendItemData) => {
-      const { items, survivor } = data;
-      const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
-
-      if (recipient && sender) {
-        // check if either of sender or recipient is already on a trade
-        if (recipient.trading.withWho !== socket.id) {
-          return socket.emit(SocketEvents.DECLINE_EXISTING_TRADE, 'This user is already on a trade');
-        }
-        // if (recipient.trading.isTrading && sender.trading.isTrading) {
+      if (recipient.trading.isTrading && sender.trading.isTrading) {
         recipient.receivingItems = items;
         sender.sendingItems = items;
 
         socket.to(recipient.socketId).emit(SocketEvents.DELIVER_ITEMS, items);
-        // }
       }
-    });
-
-    socket.on(SocketEvents.ACCEPT_TRADE, (data: Partial<SendItemData>) => {
-      const { survivor } = data;
-      const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
-
-      if (recipient && sender) {
-        if (recipient.acceptTrade) {
-          console.log('both accepted');
-          // method to update both user's inventories
-          // emit successful trade
-        }
-        sender.acceptTrade = true;
-        socket.to(recipient.socketId).emit(SocketEvents.RECIPIENT_ACKNOWLEDGE);
-        socket.emit(SocketEvents.SENDER_ACKNOWLEDGE);
-      }
-    });
-
-    socket.on(SocketEvents.DECLINE_TRADE, (data: Partial<SendItemData>) => {
-      const { survivor } = data;
-      const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
-      const recipientIndex = existingUser(survivor.id);
-      const senderIndex = existingSocket(socket.id);
-
-      if (recipient && sender) {
-        connectedUsers.splice(recipientIndex, 1, resetUser(recipient));
-        connectedUsers.splice(senderIndex, 1, resetUser(sender));
-      }
-
-      socket.to(recipient.socketId).emit(SocketEvents.DECLINE_TRADE);
-      socket.emit(SocketEvents.DECLINE_TRADE);
-      console.log(connectedUsers);
-    });
-
-    socket.on(SocketEvents.REQUEST_USER_STATUS, (data: Partial<SendItemData>) => {
-      const { survivor } = data;
-      const user = existingUser(survivor.id);
-      socket.emit(SocketEvents.REQUEST_USER_STATUS, { status: user !== -1 });
-    });
-
-    socket.on(SocketEvents.DISCONNECT, (reason: string) => {
-      const user = existingSocket(socket.id);
-      if (user !== -1) connectedUsers.splice(user, 1);
-      console.log(`disconnected : ${socket.id} ${reason}`);
-      console.log(connectedUsers);
-    });
+    }
   });
-} catch (error) {
-  console.log('socket problem', error);
-}
+
+  socket.on(SocketEvents.ACCEPT_TRADE, (data: Partial<SendItemData>) => {
+    const { survivor } = data;
+    const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
+
+    if (recipient && sender) {
+      if (recipient.acceptTrade) {
+        console.log('both accepted');
+        // method to update both user's inventories
+        // emit successful trade
+      }
+      sender.acceptTrade = true;
+      socket.to(recipient.socketId).emit(SocketEvents.RECIPIENT_ACKNOWLEDGE);
+      socket.emit(SocketEvents.SENDER_ACKNOWLEDGE);
+    }
+  });
+
+  socket.on(SocketEvents.DECLINE_TRADE, (data: Partial<SendItemData>) => {
+    const { survivor } = data;
+    const [recipient, sender] = retrieveUsers(survivor.id, socket.id);
+    const recipientIndex = existingUser(survivor.id);
+    const senderIndex = existingSocket(socket.id);
+
+    if (recipient && sender) {
+      connectedUsers.splice(recipientIndex, 1, resetUser(recipient));
+      connectedUsers.splice(senderIndex, 1, resetUser(sender));
+    }
+
+    socket.to(recipient.socketId).emit(SocketEvents.DECLINE_TRADE);
+    socket.emit(SocketEvents.DECLINE_TRADE);
+    console.log(connectedUsers);
+  });
+
+  socket.on(SocketEvents.REQUEST_USER_STATUS, (data: Partial<SendItemData>) => {
+    const { survivor } = data;
+    const user = existingUser(survivor.id);
+    socket.emit(SocketEvents.REQUEST_USER_STATUS, { status: user !== -1 });
+  });
+
+  socket.on(SocketEvents.DISCONNECT, (reason: string) => {
+    const user = existingSocket(socket.id);
+    if (user !== -1) connectedUsers.splice(user, 1);
+    console.log(`disconnected : ${socket.id} ${reason}`);
+    console.log(connectedUsers);
+  });
+});
